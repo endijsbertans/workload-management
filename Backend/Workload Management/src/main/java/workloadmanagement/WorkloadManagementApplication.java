9e8316cd15c9c8005c3f807f0492ef2887fcf732
endijsbertans <endijsbertans@gmail.com>
2025-01-24 16:49:05 +0200

    add backend/ with start of springbook backend server

---
+++ Backend/Workload Management/src/main/java/workloadmanagement/WorkloadManagementApplication.java
@@ -0,0 +1,13 @@
+package workloadmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloadManagementApplication.class, args);
+	}
+
+}
---
+++ Backend/Workload Management/src/test/java/workloadmanagement/WorkloadManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package workloadmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkloadManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
