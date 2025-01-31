54e0be9df687e7ce820ba0ea1221d2671a11455a
endijsbertans <endijsbertans@gmail.com>
2025-01-31 22:44:21 +0200

    Removed old workload list and updated to dynamic workload list which uses angular materials mat-table

--- Frontend/workload-management/src/app/pages/workload-list/workload-list.component.ts
+++ Frontend/workload-management/src/app/pages/workload-list/workload-list.component.ts
@@ -1,25 +1,39 @@
-import {Component, inject} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
 import {WorkloadService} from "../../services/services";
 import {Router} from "@angular/router";
 import {PageResponseWorkloadResponse} from "../../services/models/page-response-workload-response";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {WorkloadResponse} from "../../services/models/workload-response";
-import {findAllWorkloads} from "../../services/fn/workload/find-all-workloads";
+import {MatSort, MatSortModule} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-workload-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatSortModule
+  ],
   templateUrl: './workload-list.component.html',
   styleUrl: './workload-list.component.scss'
 })
-export class WorkloadListComponent {
+export class WorkloadListComponent implements OnInit, AfterViewInit{
   private readonly workloadService = inject(WorkloadService);
   private readonly router = inject(Router);
-  workloadResponse: PageResponseWorkloadResponse = {};
+  workloadPageResponse: PageResponseWorkloadResponse = {};
+  dataSource = new MatTableDataSource<WorkloadResponse>([]);
+  workloadResponse?: WorkloadResponse[];
+  columnsToDisplay = ['semester','course.courseName',"teachingStaff.user.name"];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   public page = 0;
   public size = 10;
   ngOnInit(){
     this.findAllWorkloads();
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
   private findAllWorkloads() {
     this.workloadService.findAllWorkloads({
@@ -28,10 +42,19 @@ export class WorkloadListComponent {
     }).subscribe({
       next: (workloads) => {
         if (workloads.content) {
-          console.log(workloads.content[0]);
+
+          this.dataSource.data = workloads.content;
+          console.log(this.dataSource.data);
         }
-        this.workloadResponse = workloads;
+        this.workloadResponse = workloads.content;
       }
     });
   }
+  getNestedProperty(obj: any, key: string, defaultValue: any = "") {
+    let name = key.split('.')
+        .reduce((acc, part) => acc?.[part], obj) ?? defaultValue;
+
+    return name;
+  }
+
 }
