5166512c764ca0c323210c94a47101957de8bf1c
endijsbertans <endijsbertans@gmail.com>
2025-02-02 17:04:34 +0200

    Added collapsable columns

---
+++ Frontend/workload-management/src/app/pages/workload-list/workload-list-columns.ts
@@ -0,0 +1,160 @@
+export interface ColumnNames {
+  name: string,
+  pathTo: string,
+  collection: string
+}
+
+export const ColumnsForTeacher: ColumnNames[] = [
+  {
+    name: 'Vārds',
+    pathTo: 'teachingStaff.user.name',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Uzvārds',
+    pathTo: 'teachingStaff.user.surname',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'KP pilnai slodzei',
+    pathTo: 'cpForFullTime',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Slodzes daļa',
+    pathTo: 'cpProportionOnFullTime',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Amata nosaukums',
+    pathTo: 'teachingStaff.positionTitle',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Amata grupa',
+    pathTo: 'teachingStaff.staffAcademicRank.rankName',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Statuss',
+    pathTo: 'statusType.statusTypeName',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Iekļaut budžetā',
+    pathTo: 'includeInBudget',
+    collection: 'ColumnsForTeacher'
+  },
+  {
+    name: 'Pasniedzēja fakultāte',
+    pathTo: 'teachingStaff.staffFaculty.facultyName',
+    collection: 'ColumnsForTeacher'
+  },
+];
+
+export const ColumnsForCourse: ColumnNames[] = [
+
+  {
+    name: 'Semestris',
+    pathTo: 'semester',
+    collection: 'ColumnsForCourse'
+  }
+  , {
+    name: 'Priekšmeta nosaukums',
+    pathTo: 'course.courseName',
+    collection: 'ColumnsForCourse'
+
+  }
+  , {
+    name: 'Programmas Daļa',
+    pathTo: 'course.section',
+    collection: 'ColumnsForCourse'
+  }
+  , {
+    name: 'Reģistrācija',
+    pathTo: 'course.registrationType',
+    collection: 'ColumnsForCourse'
+  }
+  , {
+    name: 'Lais kods',
+    pathTo: 'course.courseCode',
+    collection: 'ColumnsForCourse'
+  }
+];
+export const ColumnsForCalc: ColumnNames[] = [
+  {
+    name: 'KP/stundas',
+    pathTo: 'creditPointsPerHour',
+    collection: 'ColumnsForCalc'
+  }
+  , {
+    name: 'Priekšmeta KP',
+    pathTo: 'course.creditPoints',
+    collection: 'ColumnsForCalc'
+  }
+  , {
+    name: 'KP skaits grupai',
+    pathTo: 'creditPointsPerGroup',
+    collection: 'ColumnsForCalc'
+  }
+  , {
+    name: 'Grupu skaits',
+    pathTo: 'groupAmount',
+    collection: 'ColumnsForCalc'
+  },
+  {
+    name: 'Kontakt stundas',
+    pathTo: 'contactHours',
+    collection: 'ColumnsForCalc'
+  }
+
+];
+export const ColumnsForGeneralInfo: ColumnNames[] = [
+   {
+    name: 'Kursi',
+    pathTo: 'myClasses',
+     collection: 'ColumnsForGeneralInfo'
+  }
+  , {
+    name: 'Komentāri',
+    pathTo: 'comments',
+    collection: 'ColumnsForGeneralInfo'
+  }
+  ]
+export const ColumnsForSalary: ColumnNames[] = [
+ {
+    name: 'Budžeta pozīcija',
+    pathTo: 'budgetPosition',
+   collection: 'ColumnsForSalary'
+  }
+  , {
+    name: 'Alga',
+    pathTo: 'teachingStaff.staffAcademicRank.salary',
+    collection: 'ColumnsForSalary'
+  }
+  , {
+    name: 'Nozares koeficents',
+    pathTo: 'industryCoefficient',
+    collection: 'ColumnsForSalary'
+  }
+  , {
+    name: 'Alga mēnesī',
+    pathTo: 'salaryPerMonth',
+    collection: 'ColumnsForSalary'
+  }
+  , {
+    name: 'Vai atvaļinājums ieskaitās',
+    pathTo: 'vacationMonths',
+    collection: 'ColumnsForSalary'
+  }
+  , {
+    name: 'Mēnešu skaits',
+    pathTo: 'monthSum',
+    collection: 'ColumnsForSalary'
+  }
+  , {
+    name: 'Algai paredzēts',
+    pathTo: 'expectedSalary',
+    collection: 'ColumnsForSalary'
+  }
+]
--- Frontend/workload-management/src/app/pages/workload-list/workload-list.component.ts
+++ Frontend/workload-management/src/app/pages/workload-list/workload-list.component.ts
@@ -1,18 +1,30 @@
-import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, inject, OnInit, signal, ViewChild} from '@angular/core';
 import {WorkloadService} from "../../services/services";
 import {Router} from "@angular/router";
-import {PageResponseWorkloadResponse} from "../../services/models/page-response-workload-response";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {WorkloadResponse} from "../../services/models/workload-response";
 import {MatSort, MatSortModule} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
+import {
+  ColumnNames,
+  ColumnsForCalc,
+  ColumnsForCourse,
+  ColumnsForGeneralInfo, ColumnsForSalary,
+  ColumnsForTeacher
+} from "./workload-list-columns";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatSuffix} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-workload-list',
   standalone: true,
   imports: [
     MatTableModule,
-    MatSortModule
+    MatSortModule,
+    MatIcon,
+    MatIconButton,
+    MatSuffix
   ],
   templateUrl: './workload-list.component.html',
   styleUrl: './workload-list.component.scss'
@@ -20,16 +32,34 @@ import {MatPaginator} from "@angular/material/paginator";
 export class WorkloadListComponent implements OnInit, AfterViewInit{
   private readonly workloadService = inject(WorkloadService);
   private readonly router = inject(Router);
-  workloadPageResponse: PageResponseWorkloadResponse = {};
   dataSource = new MatTableDataSource<WorkloadResponse>([]);
   workloadResponse?: WorkloadResponse[];
-  columnsToDisplay = ['semester','course.courseName',"teachingStaff.user.name"];
+  columnsToDisplay?: ColumnNames[];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   public page = 0;
   public size = 10;
+  columnsForTeacher= signal(true);
+  columnsForCourse = signal(true);
+  columnsForCalc = signal(true);
+  columnsForGeneralInfo = signal(true);
+  columnsForSalary = signal(true);
+
+
+  clickEvent(event: MouseEvent, name: string) {
+    if(name == 'ColumnsForTeacher')this.columnsForTeacher.set(!this.columnsForTeacher());
+    if(name == 'ColumnsForCourse')this.columnsForCourse.set(!this.columnsForCourse());
+    if(name == 'ColumnsForCalc')this.columnsForCalc.set(!this.columnsForCalc());
+    if(name == 'ColumnsForGeneralInfo')this.columnsForGeneralInfo.set(!this.columnsForGeneralInfo());
+    if(name == 'ColumnsForSalary')this.columnsForSalary.set(!this.columnsForSalary());
+
+    event.preventDefault();
+    event.stopPropagation();
+    this.displayedColumns()
+  }
   ngOnInit(){
     this.findAllWorkloads();
+    this.columnsToDisplay = this.displayedColumns();
   }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -42,7 +72,6 @@ export class WorkloadListComponent implements OnInit, AfterViewInit{
     }).subscribe({
       next: (workloads) => {
         if (workloads.content) {
-
           this.dataSource.data = workloads.content;
           console.log(this.dataSource.data);
         }
@@ -50,11 +79,41 @@ export class WorkloadListComponent implements OnInit, AfterViewInit{
       }
     });
   }
-  getNestedProperty(obj: any, key: string, defaultValue: any = "") {
-    let name = key.split('.')
-        .reduce((acc, part) => acc?.[part], obj) ?? defaultValue;
 
-    return name;
+  getNestedProperty(obj: any, key: string, defaultValue: any = "") {
+    if(key == "myClasses"){
+      let result: string[] = [];
+      let val = this.digInObject(obj, key);
+      val.forEach(function(val: any){
+        result.push(val.className);
+      });
+      return result.join(', ');
+    }
+    return this.digInObject(obj, key);
   }
+  digInObject(obj: any, key: string, defaultValue: any = ""){
+    return key.split('.')
+      .reduce((acc, part) => acc?.[part], obj) ?? defaultValue;
+
+  }
+  mapDisplayedColumns():string[]{
+    return this.displayedColumns().map(col => col.pathTo);
+
+  }
+  displayedColumns(): ColumnNames[] {
+    let columns: ColumnNames[] = [];
+    if (this.columnsForTeacher()) columns.push(...ColumnsForTeacher);
+    else{
+      const fallbackColumn = ColumnsForTeacher.at(0);
+      if (fallbackColumn) columns.push(fallbackColumn);
+    }
+    if (this.columnsForCourse()) columns.push(...ColumnsForCourse);
+    if (this.columnsForCalc()) columns.push(...ColumnsForCalc);
+    if (this.columnsForGeneralInfo()) columns.push(...ColumnsForGeneralInfo);
+    if (this.columnsForSalary()) columns.push(...ColumnsForSalary);
+    return columns;
+  }
+
+
 
 }
