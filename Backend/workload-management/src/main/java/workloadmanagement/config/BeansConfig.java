8bbaacc44ab92058daade1ec5167eb7464e1ff22
endijsbertans <endijsbertans@gmail.com>
2025-03-06 10:47:02 +0200

    add: docker config files, github pipeline 1st try at making CI/CD pipeline

--- Backend/workload-management/src/main/java/workloadmanagement/config/BeansConfig.java
+++ Backend/workload-management/src/main/java/workloadmanagement/config/BeansConfig.java
@@ -2,6 +2,7 @@ package workloadmanagement.config;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -19,12 +20,15 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.http.HttpHeaders.*;
 @Configuration
 @RequiredArgsConstructor
 public class BeansConfig {
     private final UserDetailsService userDetailsService;
+    @Value("${application.cors.origins}")
+    private List<String> allowedOrigins;
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -44,8 +48,8 @@ public class BeansConfig {
     public CorsFilter corsFilter(){
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        //config.setAllowCredentials(true);
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedHeaders(Arrays.asList(
                 ORIGIN,
                 CONTENT_TYPE,
